a68ecf2e08386305e2fe588b36ecf96a9d34e67b
user9592844 <139545179+user9592844@users.noreply.github.com>
2025-05-12 23:19:19 +0800

    Chore: Flake updates

--- flake.nix
+++ flake.nix
@@ -4,16 +4,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     disko.url = "github:nix-community/disko";
+    impermanence.url = "github:nix-community/impermanence";
     home-manager.url = "github:nix-community/home-manager";
     sops.url = "github:mic92/sops-nix";
+
+    # Private repository to hold the secrets for hosts/systems
     duat-secrets = {
       url = "git+ssh://git@github.com/user9592844/duat-secrets?shallow=1&ref=main";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, disko, home-manager, sops, duat-secrets, ... }@inputs:
+  outputs = { self, nixpkgs, disko, impermanence, home-manager, sops, duat-secrets, ... }@inputs:
     let
+      # Allow custom library to be under lib
       lib = nixpkgs.lib.extend
         (self: super: { custom = import ./lib { inherit (nixpkgs) lib; }; });
       forEachHost =
@@ -24,7 +28,7 @@
       # NOTE: Only the currently requested hostname will be built
       nixosConfigurations = forEachHost (host:
         lib.nixosSystem {
-          specialArgs = { inherit inputs lib disko sops home-manager duat-secrets; };
+          specialArgs = { inherit inputs lib disko sops home-manager impermanence duat-secrets; };
           modules = [ ./hosts/hosts/${host} ];
         });
     };
