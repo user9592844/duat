ba403a8d667f24870933778eda1c79e22f4bdbc7
user9592844 <139545179+user9592844@users.noreply.github.com>
2025-10-13 10:59:29 +0800

    Fix: Moved imhotep sops secrets to maat only

--- home/imhotep/core/sops.nix
+++ home/imhotep/core/sops.nix
@@ -10,21 +10,6 @@ in
     defaultSopsFile = "${secretsFile}";
     validateSopsFiles = false;
 
-    secrets = {
-      "ssh/private_9673d1" = {
-        path = "${config.home.homeDirectory}/.ssh/config.d/private.9673d1";
-        mode = "0400";
-      };
-
-      "ssh/id_ed25519_9673d1" = {
-        path = "${config.home.homeDirectory}/.ssh/id_ed25519_9673d1";
-        mode = "0600";
-      };
-
-      "ssh/id_ed25519_99cd21" = {
-        path = "${config.home.homeDirectory}/.ssh/id_ed25519_99cd21";
-        mode = "0600";
-      };
-    };
+    secrets = { };
   };
 }
--- home/imhotep/maat.nix
+++ home/imhotep/maat.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{ lib, sops, config, ... }: {
   imports = [
     ./common.nix
   ] ++ map lib.custom.relativeToRoot [
@@ -8,4 +8,24 @@
 
     "home/imhotep/optional/terminal/ghostty.nix"
   ];
+
+  # These secrets are only desired on maat
+  sops = {
+    secrets = {
+      "ssh/private_9673d1" = {
+        path = "${config.home.homeDirectory}/.ssh/config.d/private.9673d1";
+        mode = "0400";
+      };
+
+      "ssh/id_ed25519_9673d1" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519_9673d1";
+        mode = "0600";
+      };
+
+      "ssh/id_ed25519_99cd21" = {
+        path = "${config.home.homeDirectory}/.ssh/id_ed25519_99cd21";
+        mode = "0600";
+      };
+    };
+  };
 }
