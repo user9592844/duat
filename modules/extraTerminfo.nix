b4d6e41a073b3c4dd34425b1afe66a7284abd661
user9592844 <139545179+user9592844@users.noreply.github.com>
2025-12-08 12:17:42 +0800

    Fix: Updates from PR linting checks

--- modules/extraTerminfo.nix
+++ modules/extraTerminfo.nix
@@ -1,11 +1,6 @@
 # This module manages the terminfo database
 # and its integration in the system.
-{ config
-, lib
-, pkgs
-, ...
-}:
-{
+{ config, lib, pkgs, ... }: {
 
   options = {
     environment.extraTerminfo = lib.mkOption {
@@ -19,35 +14,25 @@
 
   config = {
 
-    # This should not contain packages that are broken or can't build, since it
-    # will break this expression
-    #
-    # can be generated with:
-    # lib.attrNames (lib.filterAttrs
-    #  (_: drv: (builtins.tryEval (lib.isDerivation drv && drv ? terminfo)).value)
-    #  pkgs)
-    environment.systemPackages = lib.mkIf (config.environment.extraTerminfo != [ ]) (
-      map
-        (name: (builtins.getAttr name pkgs.pkgsBuildBuild).terminfo)
-        config.environment.extraTerminfo
-    );
-
-    environment.pathsToLink = [
-      "/share/terminfo"
-    ];
-
-    environment.etc.terminfo = {
-      source = "${config.system.path}/share/terminfo";
+    environment = {
+      systemPackages = lib.mkIf (config.environment.extraTerminfo != [ ]) (
+        map
+          (name: (builtins.getAttr name pkgs.pkgsBuildBuild).terminfo)
+          config.environment.extraTerminfo
+      );
+      pathsToLink = [
+        "/share/terminfo"
+      ];
+      etc.terminfo = {
+        source = "${config.system.path}/share/terminfo";
+      };
+      profileRelativeSessionVariables = {
+        TERMINFO_DIRS = [ "/share/terminfo" ];
+      };
+      extraInit = ''
+        # reset TERM with new TERMINFO available (if any)
+        export TERM=$TERM
+      '';
     };
-
-    environment.profileRelativeSessionVariables = {
-      TERMINFO_DIRS = [ "/share/terminfo" ];
-    };
-
-    environment.extraInit = ''
-
-      # reset TERM with new TERMINFO available (if any)
-      export TERM=$TERM
-    '';
   };
 }
