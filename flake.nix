20406e172247445fe4e23dae7276a16cf9e73fe7
user9592844 <139545179+user9592844@users.noreply.github.com>
2025-12-08 12:55:31 +0800

    Chore: Change to Stable-Small Branch
    - Change nixpkgs to nixos-25.11-small
    - Change home-manager to release-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11-small";
     nixos-hardware.url = "github:NixOs/nixos-hardware";
     disko = {
       url = "github:nix-community/disko";
@@ -10,7 +10,7 @@
     };
     impermanence.url = "github:nix-community/impermanence";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops = {
